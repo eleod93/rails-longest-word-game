04a878cb34b34ca94fe178cc326d4eebdab241a5
Eleonore Delaire <eleonore.delaire@edu.escpeurope.eu>
2017-05-15 17:31:23 +0200

    longuest game exercice

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/game' => 'gaming#game'
+  get '/score' => 'gaming#score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
